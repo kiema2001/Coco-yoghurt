8393e63447d130f47906516a340a47f74b350c74
Henry Kiema <henrykiema47@gmail.com>
2025-08-20 12:22:23 +0300

    Add App.jsx to fix Vercel build

---
+++ client/src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import StaffDashboard from "./pages/StaffDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow container mx-auto px-4 py-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/staff"
+              element={
+                <ProtectedRoute>
+                  <StaffDashboard />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
